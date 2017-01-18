1a72c4f176910fe574f479438aeee2f08bdafde0
pc-79 <hiteshk764@gmail.com>
2017-01-18 20:33:36 +0530

    1

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+cout<<"enter the values of a and b ";
+cin>>a>>b;
+cout<<"values of a and b are";
+cout<<a<<b;
+return 0;
+
+}
+
